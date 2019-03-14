2fed7c519da757f87185c9be9af97cdc306cbba9
Matthias Hertel <mathertel@hotmail.com>
2019-03-14 23:18:32 +0100

    linechart update

--- lineChart.js
+++ lineChart.js
@@ -1,4 +1,5 @@
 var panelObj = document.getElementById('panel');
+var vObj = document.getElementById('v-labels');
 
 /// <reference path="microsvg.js" />
 
@@ -10,7 +11,11 @@ function maxReducer(acc, val) {
 
 var linesData = [];
 var linesMeta = []; // {maxX:12, minX:0 }
-var gScale= {minX: 0,maxX: 1,minY: 0,maxY: 1};
+var linesDraw = []; // true/false needs redraw
+
+var minBox = { minX: Infinity, maxX: -Infinity, minY: 0, maxY: 1 };
+var dataBox = minBox;
+var displayBox = minBox;
 
 // look in all linesData for maximum values
 
@@ -48,7 +53,7 @@ function drawLine(lineID) {
   }
 
   var values = linesData[lineID];
-  var meta = gScale; // linesMeta[lineID];
+  var meta = displayBox; // linesMeta[lineID];
 
   var scaleX = 128 / (meta.maxX - meta.minX);
   var scaleY = 36 / (meta.maxY - meta.minY);
@@ -67,6 +72,45 @@ function drawLine(lineID) {
     id: 'line-' + lineID,
     points: points.join(' ')
   });
+}
+
+function drawY() {
+  // clear existing y-Axis
+  var YAxisGroup = document.getElementById('v-labels');
+  Array.from(YAxisGroup.childNodes).forEach(function(c) {
+    c.remove();
+  });
+
+  var dataRange = dataBox.maxY - dataBox.minY;
+  var dispRange = Math.pow(10, Math.floor(Math.log10(dataRange)));
+
+  var step;
+  if (dataRange < dispRange * 2) {
+    step = dispRange / 2;
+  } else if (dataRange < dispRange * 4) {
+    step = dispRange;
+  } else if (dataRange < dispRange * 5) {
+    step = dispRange;
+  } else if (dataRange < dispRange * 10) {
+    step = 2 * dispRange;
+  }
+
+  var high = Math.ceil(dataBox.maxY / step) * step;
+  var low = Math.floor(dataBox.minY / step) * step;
+  displayBox = Object.assign({}, dataBox, { minY: low, maxY: high });
+
+  console.log('y:', high, low);
+  var scaleY = 36 / (high - low);
+
+  var n;
+  for (n = low; n <= high; n += step) {
+    var txtObj = createSVGNode(vObj, 'text', {
+      x: 11,
+      y: -1 * (n - low) * scaleY
+    });
+    txtObj.textContent = String(n);
+    // <text x="11" y="0">0-</text>
+  }
 }
 
 function redraw(lineID) {
@@ -91,44 +135,65 @@ function addValues(lineID, values) {
 
 function addLine(values) {
   var lineID = linesData.length;
-  linesData.push(values);
-  scaleLine(lineID);
-  var newScale = linesMeta.reduce(outerBox);
-  var doAll = ((newScale.minY != gScale.minY) || (newScale.maxY != gScale.maxY));
-  gScale = newScale;
-  if (doAll) {
-    // draw all lines
-    linesData.forEach(function(d, n) {
-      drawLine(n);
-    });
-  } else {
-    // draw single line
-    drawLine(lineID);
-  }
-    return lineID;
-}
-
-function updateLine(lineID, values) {
   linesData[lineID] = values;
-  redraw(lineID);
+  linesDraw[lineID] = true;
+
+  scaleLine(lineID);
+  var newBox = linesMeta.reduce(outerBox, minBox);
+  var doAll =
+    newBox.minY != dataBox.minY ||
+    newBox.maxY != dataBox.maxY ||
+    newBox.minX != dataBox.minX ||
+    newBox.maxX != dataBox.maxX;
+
+  if (doAll) {
+    linesDraw.forEach(function(v, n) {
+      linesDraw[n] = true;
+    });
+  } // if
+
+  dataBox = newBox;
+
   return lineID;
-}
+} // addLine()
+
+function drawLines() {
+  for (var n in linesData) {
+    if (linesDraw[n]) drawLine(n);
+    linesDraw[n] = false;
+  }
+} // draw
+
+// function updateLine(lineID, values) {
+//   linesData[lineID] = values;
+
+//   redraw(lineID);
+//   return lineID;
+// }
 
 // var l1 = addLine([1,5,3,6,4,8,9,12]);
+
+function setMinScale(bBox) {
+  Object.assign(minBox, bBox);
+}
+
+// debugger;
+// setMinScale({maxY:50});
+
 
 var l1 = addLine([
   [Date.parse('2019-03-12T16:30:00'), 16],
   [Date.parse('2019-03-12T16:31:00'), 16],
   [Date.parse('2019-03-12T16:32:00'), 19],
-  [Date.parse('2019-03-12T16:33:00'), 17],
-  [Date.parse('2019-03-12T16:34:00'), 22],
-  [Date.parse('2019-03-12T16:35:00'), 21]
+  [Date.parse('2019-03-12T16:33:00'), 27],
+  [Date.parse('2019-03-12T16:34:00'), 12],
+  [Date.parse('2019-03-12T16:35:00'), -12]
 ]);
 
-var l2 = addLine([
-  [Date.parse('2019-03-12T16:30:00'), 10],
-  [Date.parse('2019-03-12T16:35:00'), 10]
-]);
+var l2 = addLine([[Date.parse('2019-03-12T16:30:00'), 0], [Date.parse('2019-03-12T16:35:00'), 0]]);
+
+drawY();
+drawLines();
 
 // addValues(l1, [12]);
 // addValues(l1, 14);
@@ -138,7 +203,7 @@ var l2 = addLine([
 document['api'] = {
   addLine: addLine,
   addValues: addValues,
-  updateLine: updateLine
+  drawLines: drawLines
 };
 
 // End.
