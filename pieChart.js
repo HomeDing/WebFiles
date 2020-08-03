4865a3e218a152d5ad1d153015b9e89b1fb52dce
Matthias Hertel <mathertel@hotmail.com>
2020-08-03 19:21:10 +0200

    svg improvements on pie chart and element API

--- pieChart.js
+++ pieChart.js
@@ -1,40 +1,58 @@
+// Pie-Chart Micro charts implementation.
+// must include microsvg.js
+//
+// Copyright (c) by Matthias Hertel, http://www.mathertel.de
+// This work is licensed under a BSD 3-Clause license.
+//
+// * 03.08.2020 include <title> for mouse over effect. 
+
 var panelObj = document.getElementById("panel");
 var RAD_OUT = 20;
 
-/** create an outer point on the circle */
-function piePoint(alpha, r) {
+
+/**
+ * Calculate an outer point on the circle, usable for svg paths
+ */
+function _piePoint(alpha, r) {
   var s = " " + (Math.sin(alpha) * r) + "," + (Math.cos(alpha) * -r);
   return (s);
-}
+} // _piePoint()
 
-function addPiePart(start, part, color) {
+
+/**
+ * Append another pie slice inside inside the panelObj.
+ */
+function _addPieSlice(start, part, color, title) {
   var alpha = 2 * Math.PI * start;
   var beta = 2 * Math.PI * (start + part);
 
-  // create gauge part path
+  var g = createSVGNode(panelObj, "g", null);
+  createSVGNode(g, "title").textContent = title;
+
+  // create pie slice path
   var p = "";
-  p += "M" + piePoint(alpha, RAD_OUT);
+  p += "M" + _piePoint(alpha, RAD_OUT);
   p += "A" + RAD_OUT + "," + RAD_OUT;
   if (part < 0.5) {
     p += " 0 0 1 ";
   } else {
     p += " 0 1 1 ";
   }
-  p += piePoint(beta, RAD_OUT);
+  p += _piePoint(beta, RAD_OUT);
   p += "L" + " 0,0";
   p += "Z";
-  createSVGNode(panelObj, "path", {
+  createSVGNode(g, "path", {
     class: "segment",
     style: "fill:" + color,
     d: p
   });
+} // _addPieSlice()
 
-} // addPiePart()
 
-/** create a set of ranges for the gauge panel in the background. The sum of all parts should add-up to 1.
- * @rangeList: List of gauge segments. 
+/** create a set of ranges for the pie chart panel in the background. The sum of all parts should add-up to 1.
+ * @rangeList: List of pie chart segments. 
  */
-function setRange(rangeList) {
+function _setRange(rangeList) {
   if (rangeList) {
     // calculate sum:
     var sum = 0;
@@ -45,15 +63,27 @@ function setRange(rangeList) {
     var x = 0;
     for (var n = 0; n < rangeList.length; n++) {
       var p = rangeList[n].part / sum;
-      addPiePart(x, p, rangeList[n].color);
+      _addPieSlice(x, p, rangeList[n].color, rangeList[n].title);
       x += p;
     } // for
   } // if
-} // setRange()
+} // _setRange()
 
 
+/**
+ * Clear the pie chart.
+ * Remove all elements inside the panelObj.
+ */
+function _clear() {
+  while (panelObj.firstChild)
+    panelObj.removeChild(panelObj.firstChild);
+} // _clear()
+
+
+// expose API functions.
 document.api = {
-  setRange: setRange
+  clear: _clear,
+  setRange: _setRange
 };
 
 // End.
