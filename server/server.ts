e05521661c9530d10bae3eb6c55d57a0abd4601b
Matthias Hertel <mathertel@hotmail.com>
2021-03-14 18:17:12 +0100

    remove deprecated http headers

--- server/server.ts
+++ server/server.ts
@@ -110,8 +110,6 @@ if (options.case) {
  */
 function noCache(_req: express.Request, res: express.Response, next: express.NextFunction) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
   next();
 } // noCache
 
