9eab8565866701bf2f366b948ab5883d9743a904
Matthias Hertel <mathertel@hotmail.com>
2024-12-07 18:08:36 +0100

    dependency updates

--- publishDoku.bat
+++ publishDoku.bat
@@ -4,7 +4,7 @@ echo *** HomeDing Update of API documentation files. ***
 echo *** This utility copies some files to the HomeDing Doku to share API data and visualisation. ***
 echo.
 
-set tar=C:\Users\Matthias\OneDrive\Dokumente\homeding
+set tar=%USERPROFILE%\Projects\homeding-docu
 
 copy elements.json %tar%
 copy element.svg %tar%
